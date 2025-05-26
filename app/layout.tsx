860807421f2f030908e50f884170580d338d72a1
Sandeep R <sandeepr@Sandeeps-MacBook-Air.local>
2025-05-26 23:36:44 +0530

    Implement Eslint & Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
