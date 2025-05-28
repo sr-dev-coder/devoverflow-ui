ae6963180d0f5d1cd14742f2b78e8a2d82114573
sandeepr <sandeeprohillainfo@gmail.com>
2025-05-28 22:57:08 +0530

    feat(navbar): navbar added on header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/theme";
 
 const interVF = localFont({
@@ -41,6 +42,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
